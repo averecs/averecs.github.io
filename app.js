cdf79e74d68dff8ee19acf6fbe49afa913d3212c
alfonso.mateos <aj.mateos97@gmail.com>
2024-07-03 00:26:25 +0100

    Implement carousel for mobile our team

--- app.js
+++ app.js
@@ -9,3 +9,21 @@ hiddenElements.forEach((el) => observer.observe(el));
 
 // TODO: finish
 // TODO: maybe create different types or animations (css class) and pass it to the observer function
+
+// Our team slide effect indicators
+document.addEventListener("DOMContentLoaded", () => {
+  const carousel = document.getElementById("our-team-slider");
+  const indicators = document.querySelectorAll(".our-team .indicator");
+
+  carousel.addEventListener("scroll", () => {
+    const scrollLeft = carousel.scrollLeft;
+    const totalWidth = carousel.scrollWidth - carousel.clientWidth;
+    const index = Math.round(
+      (scrollLeft / totalWidth) * (indicators.length - 1)
+    );
+
+    indicators.forEach((indicator, i) =>
+      indicator.classList.toggle("active", i === index)
+    );
+  });
+});
