8d0d06aed4e15f564b3247d27642c08f7a07f04d
alfonso.mateos <aj.mateos97@gmail.com>
2024-07-05 15:32:58 +0100

    Add animation, transitions, compressed images and responsive tweaks

--- app.js
+++ app.js
@@ -1,29 +1,59 @@
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    entry.target.classList.toggle("show", entry.isIntersecting);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
   });
 });
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
 
-// TODO: finish
-// TODO: maybe create different types or animations (css class) and pass it to the observer function
-
-// Our team slide effect indicators
 document.addEventListener("DOMContentLoaded", () => {
-  const carousel = document.getElementById("our-team-slider");
-  const indicators = document.querySelectorAll(".our-team .indicator");
+  // Carousel indicators
+  const carouselWhy = document.getElementById("why-slider");
+  const whyIndicators = document.querySelectorAll(".why .indicator");
+  const whyBubbles = document.querySelectorAll(".why .bubble");
+  const carouselOurTeam = document.getElementById("our-team-slider");
+  const ourTeamIndicators = document.querySelectorAll(".our-team .indicator");
+  const ourTeamCards = document.querySelectorAll(".our-team .card");
 
-  carousel.addEventListener("scroll", () => {
-    const scrollLeft = carousel.scrollLeft;
-    const totalWidth = carousel.scrollWidth - carousel.clientWidth;
+  carouselWhy.addEventListener("scroll", () => {
+    const scrollLeft = carouselWhy.scrollLeft;
+    const totalWidth = carouselWhy.scrollWidth - carouselWhy.clientWidth;
     const index = Math.round(
-      (scrollLeft / totalWidth) * (indicators.length - 1)
+      (scrollLeft / totalWidth) * (whyIndicators.length - 1)
     );
 
-    indicators.forEach((indicator, i) =>
-      indicator.classList.toggle("active", i === index)
-    );
+    whyIndicators.forEach((indicator, i) => {
+      indicator.classList.toggle("active", i === index);
+      whyBubbles[i].classList.toggle("active", i === index);
+    });
   });
+
+  carouselOurTeam.addEventListener("scroll", () => {
+    const scrollLeft = carouselOurTeam.scrollLeft;
+    const totalWidth =
+      carouselOurTeam.scrollWidth - carouselOurTeam.clientWidth;
+    const index = Math.round(
+      (scrollLeft / totalWidth) * (ourTeamIndicators.length - 1)
+    );
+
+    ourTeamIndicators.forEach((indicator, i) => {
+      indicator.classList.toggle("active", i === index);
+      ourTeamCards[i].classList.toggle("active", i === index);
+    });
+  });
+
+  // Get remaining days
+  const targetDate = "2024-12-15";
+
+  const target = new Date(targetDate);
+  const today = new Date();
+
+  const difference = target.getTime() - today.getTime();
+  const days = Math.ceil(difference / (1000 * 60 * 60 * 24));
+
+  const countdownElement = document.getElementById("countdown");
+  countdownElement.textContent = days;
 });
